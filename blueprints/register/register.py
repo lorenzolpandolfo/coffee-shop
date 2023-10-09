ce65839b418d4d3ca173c52afaa9d333391efaa8
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-10-08 23:00:51 -0300

    commit

--- blueprints/register/register.py
+++ blueprints/register/register.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, render_template, redirect, request, url_for, session
 import crud
+import user as class_user
 
-auth = crud.auth 
+db = crud.database
+auth = crud.auth
 
 register_bp = Blueprint("register", __name__, template_folder="templates")
 
 @register_bp.route("/registrar-se", methods=["POST", "GET"])
 def register():
-    return redirect(url_for("homepage.home", USER='TESTE'))
     if request.method == "POST":
         email = request.form["email"]
+        nome = request.form["nome"]
         senha = request.form["senha"]
+        telefone = request.form["telefone"]
 
         # Tem que conferir se o email ja nao ta registrado manualmente. Ou entao pegar o erro que o Firebase retorna
         if len(senha) < 6:
@@ -19,7 +22,11 @@ def register():
             try:
                 user = auth.create_user_with_email_and_password(email, senha)
                 session["user"] = auth.get_account_info(user["idToken"])
-                return redirect(url_for("homepage.home", USER='TESTE'))
+                mynewuser = class_user.User(session['user']['users'][0]['localId'], nome, email, telefone)
+                
+                db.child("users").update({mynewuser.nome:mynewuser.toDict()})
+                class_user.myuser = mynewuser
+                return redirect(url_for("homepage.home"))
             
             except Exception as ERRO:
                 return render_template("register.html", erro = ERRO)
---
+++ user.py
@@ -0,0 +1,16 @@
+class User():
+    def __init__(self, localid, nome, email, telefone):
+        self.localid = localid
+        self.nome = nome
+        self.email = email
+        self.telefone = telefone
+    
+    def toDict(self):
+        return {
+            "nome": self.nome,
+            "localid": self.localid,
+            "email": self.email,
+            "telefone": self.telefone
+        }
+myuser = User(0,0,0,0)
+
