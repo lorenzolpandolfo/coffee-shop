d8b10d7f6107510ed752e1f709841b05755c5e20
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-10-11 18:50:54 -0300

    registrando itens novos

--- add.py
+++ add.py
@@ -2,26 +2,20 @@ def adicionar_novo_item():
     titulo = input("> Digite o título do item: ")
     descricao = input("> Digite a descrição: ")
     lactose = input("> Contém lactose? (S/n): ").upper()
-    lactose = lactose in ['S', '']
+
+    yes = ['S', '']
+
+    lactose = lactose in yes
     vegan = False
 
     if lactose:
         vegan = False
+        opcoes = input("> Este item contém uma versão alternativa?\nEx.: Sem lactose, com chantilly, descafeinado...? (S/n): ").upper()
+        opcoes = opcoes in yes
     else:
         vegan = input("> É vegano? (S/n): ").upper()
-        vegan = vegan in ['S', '']
+        vegan = vegan in yes
+    
+    link = input("> Insira o link estático da imagem de destaque do item (CTRL + SHIFT + V para colar): ")
+    
 
-    print(f"Resultados:\nvegan: {vegan}\nlactose:{lactose}")
-
-    """
-    if lactose == 'S' or lactose == '':
-        lactose = True
-
-    else:
-        lactose = False
-        vegan = input("> É vegano? (S/n): ").upper()
-        if vegan == 'S' or vegan == '':
-            vegan = True
-        else:
-            vegan = False
-    """
