2d9b7e4159be7dfc4978b88ae05f1172266ed6b6
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-10-12 12:25:04 -0300

    retomando arquivos antigos
    alguns arquivos tiveram que ser excluidos do timeline de commits pois continham informações de login no firebase

---
+++ crud.py
@@ -0,0 +1,43 @@
+import firebase_settings
+
+auth = firebase_settings.auth
+database = firebase_settings.database
+storage = firebase_settings.storage
+admin_auth = firebase_settings.admin_auth
+
+import add
+
+if __name__ == '__main__':
+    keep = True
+    
+    while keep:
+        try:
+            escolha = int(input("1. Apagar todas as contas\n2. Registrar um novo item\n> Digite a opção desejada: "))
+
+            # apagar todas as contas (apenas para testes de registro de usuario)
+            if escolha == 1:
+                userlist = admin_auth.list_users().iterate_all()
+                for user in userlist:
+                    admin_auth.delete_user(user.uid)
+                    print("deletado!")
+                    
+            # registrar um novo item
+            elif escolha == 2:
+                item = add.criar_novo_item()
+                if item:
+                    print("[-] Item criado com sucesso. Enviando para o banco de dados...")
+                    print(add.adicionar_novo_item(item))
+
+            # Colocar item em stand-by (falta de estoque)
+            elif escolha == 3:
+                pass
+            
+            # Remover completamente um item
+            elif escolha == 4: 
+                pass
+            
+            else:
+                keep = False
+        
+        except Exception as erro:
+            print(f"[X] Erro: {erro}\n")
---
+++ firebase_settings.py
@@ -0,0 +1,21 @@
+import pyrebase
+import firebase_admin
+from firebase_admin import auth as admin_auth
+from firebase_admin import credentials
+from firebase_admin import exceptions
+import json
+
+
+with open('firebaseconfig.json', 'r') as arquivo:
+    dados_secretos = json.load(arquivo)
+
+firebaseConfig = dados_secretos
+
+firebase = pyrebase.pyrebase.initialize_app(firebaseConfig)
+
+cred = credentials.Certificate("servicekey.json")
+firebase_admin.initialize_app(cred, {"databaseURL": dados_secretos["databaseURL"]})
+
+auth = firebase.auth()
+database = firebase.database()
+storage = firebase.storage()
