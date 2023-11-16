245c9a89b5d53f9c558ccca53891f997ec277b08
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-11-16 17:03:39 -0300

    subtotal recalc

--- static/add_n_remove_cart.js
+++ static/add_n_remove_cart.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('.btn').click(function(){
+    $('.add_button').click(function(){
         var buttonValue = $(this).attr('value');
         $.ajax({
             url:'',
@@ -17,6 +17,7 @@ $(document).ready(function(){
     
     $('.remove_button').click(function(){
         var buttonValue = $(this).attr('value');
+        var self = this;
         $.ajax({
             url: '',
             type: 'get',
@@ -25,20 +26,33 @@ $(document).ready(function(){
                 item: buttonValue
             },
             success: function(response) {
+                var subtotal_price = parseFloat($('.subtotal').text());
+                var thisItemId = self.parentNode.id
+                var thisItemPrice = parseFloat($("#" + thisItemId + ".itemPrice").text().replace("R$", ''));
+                
+                var newSubtotal = parseFloat(subtotal_price - thisItemPrice);
+
+                console.log('subtotal_price:', subtotal_price);
+                console.log('thisItemId:', thisItemId);
+                console.log('thisItemPrice:', thisItemPrice);
+                console.log('newSubtotal:', newSubtotal);
+
+                $('.subtotal').text(newSubtotal)
 
                 if (response.quantidade_carrinho > 0) {
                     $('.item_count').text("Carrinho (" + response.quantidade_carrinho + ")")
-                    $('.carrinho_vazio').hide();
+                    $('.carrinho_vazio').hide()
                     $('.interagir_carrinho').show()
                 }
                 else {
                     $('.item_count').text("Carrinho")
-                    $('.carrinho_vazio').show();
+                    $('.carrinho_vazio').show()
                     $('.interagir_carrinho').hide()
                 }
+                
+                self.parentNode.remove()
             }
         })
-        this.parentNode.remove()
         
     })
 })
