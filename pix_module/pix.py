447912c45540756c4607ab28b4a5594601d6bff2
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-12-08 11:50:56 -0300

    pix qrcode works

--- pix_module/pix.py
+++ pix_module/pix.py
@@ -3,30 +3,38 @@ from PIL import Image
 from pypix import Pix
 import uuid
 
-# Criar uma instância da classe Pix
-pix = Pix()
 
-# Definir os dados do Pix
-pix.set_pixkey("lorenzopandolfo2004@gmail.com")
-pix.set_description("Lo-Coffee shop")
-pix.set_merchant_name("Lo-Cafe")
-pix.set_merchant_city("Porto Alegre")
-pix.set_country_code("BR")
-pix.set_txid(str(uuid.uuid4))
-pix.set_amount(3.1)
+def criar_qr_code(preco:float):
+    # Criar uma instância da classe Pix
+    pix = Pix()
 
-# Gerar o QR Code com Pix
-payload = str(pix)
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data(payload)
-qr.make(fit=True)
+    id = str(int(uuid.uuid4()))[:15]
 
-# Salvar o QR Code como uma imagem
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("static/pix_qr_code.png")
-img.show()
+    # Definir os dados do Pix
+    pix.set_pixkey("lorenzopandolfo2004@gmail.com")
+    pix.set_description("Lo-Coffee shop")
+    pix.set_merchant_name("Lo-Cafe")
+    pix.set_merchant_city("Porto Alegre")
+    pix.set_country_code("BR")
+    pix.set_txid(id)
+    pix.set_amount(preco)
+
+    # Gerar o QR Code com Pix
+    payload = str(pix)
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(payload)
+    qr.make(fit=True)
+
+    print(payload)
+
+    # Salvar o QR Code como uma imagem
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("pix_qr_code.png")
+    img.show()
+
+criar_qr_code(10)
