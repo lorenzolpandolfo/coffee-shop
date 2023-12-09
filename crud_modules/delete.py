377d93a4295cfeb44e0e21d35671afa7d1f7a1dc
Lorenzo Pandolfo <133379234+lorenzolpandolfo@users.noreply.github.com>
2023-12-09 12:19:29 -0300

    crud update db

--- crud_modules/delete.py
+++ crud_modules/delete.py
@@ -1,10 +1,11 @@
 from crud_modules import firebase_settings, read
-database = firebase_settings.database
+database = firebase_settings.admin_db
 
 def delete(item):
     try:
-        database.child("itens").child(item).remove()
-        return f"[!] O item {item} foi removido com sucesso.\n"
+        item_to_remove_id = read.ler_todos_itens(False, item)
+        item_to_remove = database.reference(f"itens/{item_to_remove_id}").delete()
+        return f"[!] O item foi removido com sucesso.\n"
     
     except Exception as erro:
         return f"[X] Houve um erro ao remover o item ao banco de dados.\n--> {erro}\n"
@@ -13,5 +14,5 @@ def delete(item):
 def selecionar_item():
     print('-'*26 + ' Deletando Itens ' + '-'*26)
     read.ler_todos_itens()
-    escolha = input("> Digite o nome do item para ser excluído: ")
+    escolha = int(input("> Digite o número do item para ser excluído: "))
     return escolha
--- crud_modules/read.py
+++ crud_modules/read.py
@@ -1,11 +1,25 @@
 from crud_modules import firebase_settings
-database = firebase_settings.database
+database = firebase_settings.admin_db
 
 
-def ler_todos_itens():
-    all_itens = database.child('itens').get().val()
+def ler_todos_itens(read=True, uid_get_by_title=None):
+    all_itens = database.reference("itens")
+    all_itens = all_itens.get()
+
+    uuids = []
     
-    for item in all_itens:
-        print(f"• {item}")
+    for i, itemId in enumerate(all_itens):
+        uuids.append(itemId)
+        
+        item = all_itens[itemId]
+        
+        if read:
+            print(f"{i} - {item['titulo']}")
+
+    # se eu quiser saber o uid de um item com titulo especifico
+    if uid_get_by_title is not None:            
+        return uuids[uid_get_by_title]
+        
     
-    return 0
+    else:
+        return all_itens
